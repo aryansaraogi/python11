5eba689275dc96a301c6a378dfc45b7eb4234a93
aryansaraogi <aryansaraogi02@gmail.com>
2026-04-09 23:41:37 +0530

    oops

--- oops.py
+++ oops.py
@@ -17,6 +17,9 @@ class Microwave:
 bosch:Microwave = Microwave("Bosch", 1000)
 print(bosch)
 
+perfect:Microwave = Microwave("Perfect", 1200)
+print(perfect)  
+
 
 smeg:Microwave = Microwave("Smeg", 800)
 print(smeg)
