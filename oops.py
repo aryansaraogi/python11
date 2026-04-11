77157a8fe7bb20d9f2cca7e56618fe668c0d4669
aryansaraogi <aryansaraogi02@gmail.com>
2026-04-12 00:01:19 +0530

    change

--- oops.py
+++ oops.py
@@ -13,14 +13,6 @@ class Microwave:
         self.size = 20
 
   
-
-bosch:Microwave = Microwave("Bosch", 1000)
-print(bosch)
-
-perfect:Microwave = Microwave("Perfect", 1200)
-print(perfect)  
-
-
 smeg:Microwave = Microwave("Smeg", 800)
 print(smeg)
 
