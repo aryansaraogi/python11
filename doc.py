c86fc6d2485d000607b2fa977a26383f81b7e0b2
aryansaraogi <aryansaraogi02@gmail.com>
2026-04-18 23:55:45 +0530

    changes

--- doc.py
+++ doc.py
@@ -2,12 +2,12 @@ first = 'john'
 last = 'doe'
  
 age = 30
-
+company = 'google'
 
 car = 'volvo'
 country = 'sweden'
 
-msg = f"{first} {last} drives a {car} from {country}."
+msg = f"{first} {last} is {age} years old and works at {company}. He drives a {car} from {country}."
 
 print(msg)
 
