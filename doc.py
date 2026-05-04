2964dde834e2b75cf2729f708cbb927b7f1a4508
aryansaraogi <aryansaraogi02@gmail.com>
2026-05-04 23:40:52 +0530

    hacker

--- doc.py
+++ doc.py
@@ -10,9 +10,17 @@ car = 'tesla model 3'
 country = 'usa'
 
 
+ethical_hacker = True   
+ethical_hacker_experience = 5  # years of experience as an ethical hacker       
+
+etnicity = 'hispanic'   
+
+
+
+
 BUILDING = 'empire state building'
 
-msg = f"{first} {last} is   {age} years old and works at {company}. He lives in {residence} and drives a {car} from {country}."
+msg = f"{first} {last} is a {age}-year-old ethical hacker with {ethical_hacker_experience} years of experience. He works at {company} and holds a {education} degree. He resides in {residence}, owns a {car}, and is from the {country}. He is of {etnicity} ethnicity. He has a passion for cybersecurity and enjoys exploring the latest technologies in the field. In his free time, he likes to visit iconic landmarks such as the {BUILDING}. "
 
 print(msg)
 
