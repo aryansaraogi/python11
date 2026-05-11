b24b92e10f8a4299f82f10642c606945b8482b81
aryansaraogi <aryansaraogi02@gmail.com>
2026-05-11 22:40:18 +0530

    Add car dictionary and print details for Tesla Model 3

---
+++ dic.py
@@ -0,0 +1,13 @@
+car = {
+    "brand": "tesla",
+    "model": "model 3",
+    "color": "red",
+    "year": 2020,
+    "price": 35000
+}
+
+if  car["brand"] == "tesla":
+    print(car["model"],   car["color"], car["year"], car["price"])
+
+
+ # Output: tesla
